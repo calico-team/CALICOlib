a7ae51f184d61631ae0b5f9bdb9be5c3b6800417
Connor Chang <connorc0627@gmail.com>
2025-10-17 20:20:02 -0700

    chore: bump version to 0.1.14

--- calico_lib/__init__.py
+++ calico_lib/__init__.py
@@ -1,6 +1,6 @@
 """CALICO lib for all your problem writing needs"""
 
-__version__ = "0.1.13"
+__version__ = "0.1.14"
 
 from .problem import Problem, TestFileBase, Subproblem
 from .contest import Contest
